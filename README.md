b448e4bd767ca38615cb9c09e9c00f01c971e137
andro <andrezh_gln7@live.com>
2020-12-10 17:34:55 -0500

    Initial version

--- README.md
+++ README.md
@@ -1,17 +1,17 @@
-# basic-flask-template
-A barebone Flask app using Flask and Bootstrap (v4.4.1).
+## Flask server for cp project
 
+We only require flask and flask-sijax 
 
-![alt text](https://github.com/petersimeth/basic-flask-template/blob/master/flask_screenshot.png?raw=true)
+![alt text](screen.png)
 
 ## Running Locally
 
 Make sure you have Python installed
 
 ```sh
-$ git clone https://github.com/petersimeth/basic-flask-template.git # or clone your own fork
+$ git clone https://github.com/and27/cp_project_aco_arduino
 $ cd basic-flask-template
-$ pip install -r requirements.txt
+$ pip install -r requirements.txt 
 $ python app.py
 ```
 
--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from flask import Flask, render_template
+from flask import Flask, render_template, g
+import flask_sijax 
+import os
+import time
 
 DEVELOPMENT_ENV  = True
 
@@ -16,11 +19,50 @@ app_data = {
     "keywords":     "flask, webapp, template, basic"
 }
 
+path = os.path.join('.', os.path.dirname(__file__), 'static/js/sijax/')
+app.config['SIJAX_STATIC_PATH'] = path
+flask_sijax.Sijax(app)
+
 
 @app.route('/')
 def index():
     return render_template('index.html', app_data=app_data)
 
+@flask_sijax.route(app, '/ardu/<int:req>')
+def ardu(req):
+    if req == 100:
+        with open("test.txt", 'w') as f:
+                f.write(str(100))
+                f.close()
+                time.sleep(1)
+    elif req == 0:
+        with open("test.txt", 'w') as f:
+                f.write(str(0))
+                f.close()
+
+    content = None
+    def retrieve_data(obj_response):
+        with open("test.txt", "r") as f:
+            content = f.read()
+            if int(content) == 100:
+                obj_response.html("#element", "We have <br>a new node:")
+                obj_response.css("#img_element", "display","block")
+                obj_response.script("$('#exampleModal').modal('show');")
+                obj_response.script("routes()")                
+            else:
+                obj_response.script("$('#exampleModal').modal('hide')")
+
+
+        f.close()
+        with open("test.txt", 'w') as f:
+                f.write(str(0))
+                f.close()
+
+    if g.sijax.is_sijax_request:
+        g.sijax.register_callback('ardu', retrieve_data) 
+        return g.sijax.process_request()
+          
+    return render_template("ardu.html")
 
 @app.route('/about')
 def about():
