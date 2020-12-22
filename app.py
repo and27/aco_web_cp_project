8d9cde1c113c7e6801711e675a9d4cbe57ecfae0
and27 <andrezh_gln7@live.com>
2020-12-21 21:53:41 -0500

    Se eliminan archvios no necesarios y se cambia la ruta del index

--- app.py
+++ app.py
@@ -5,30 +5,134 @@ from flask import Flask, render_template, g
 import flask_sijax 
 import os
 import time
+import math
+import random
 
 DEVELOPMENT_ENV  = True
 
 app = Flask(__name__)
-
-app_data = {
-    "name":         "Peter's Starter Template for a Flask Web App",
-    "description":  "A basic Flask app using bootstrap for layout",
-    "author":       "Peter Simeth",
-    "html_title":   "Peter's Starter Template for a Flask Web App",
-    "project_name": "Starter Template",
-    "keywords":     "flask, webapp, template, basic"
-}
 
 path = os.path.join('.', os.path.dirname(__file__), 'static/js/sijax/')
 app.config['SIJAX_STATIC_PATH'] = path
 flask_sijax.Sijax(app)
 
 
-@app.route('/')
-def index():
-    return render_template('index.html', app_data=app_data)
+class Grafo(object):
+    def __init__(self, matriz_adjacencia, rank):
+        self.matriz = matriz_adjacencia
+        self.rank = rank
+        self.feromonio = [[1 / (rank * rank) for j in range(rank)]
+                          for i in range(rank)]  # m x m
 
-@flask_sijax.route(app, '/ardu/<int:req>')
+#ACO algorithm implementation
+class ACO(object):
+    def __init__(self, cont_formiga, geracoes, alfa, beta, ro, Q=0.0):
+        self.Q = Q
+        self.ro = ro
+        self.beta = beta
+        self.alfa = alfa
+        self.cont_formiga = cont_formiga
+        self.geracoes = geracoes
+
+    def resolve(self, grafo):
+            melhor_custo = float('inf')
+            melhor_solucao = []
+            for gen in range(self.geracoes):
+                formigas = [
+                    _Ant(self, grafo) for i in range(self.cont_formiga)
+                ]
+                for ant in formigas:
+                    for i in range(grafo.rank - 1):
+                        ant._seleciona_proximo()
+                    ant.custo_total += grafo.matriz[ant.tabu[-1]][  # retorno para cidade inicial
+                        ant.tabu[0]]
+                    if ant.custo_total < melhor_custo:
+                        melhor_custo = ant.custo_total
+                        melhor_solucao = [] + ant.tabu
+                    #ant._atualiza_feromonio_delta()  # atualiza feromonio
+                #self._atualiza_feromonio(grafo, formigas)
+            return melhor_solucao, melhor_custo
+
+class _Ant(object):
+    def __init__(self, aco, grafo):
+
+        self.colonia = aco
+        self.grafo = grafo
+        self.custo_total = 0.0  # Lk
+        self.tabu = []  # caminho escolhido pela ant em uma geração
+        self.feromonio_delta = []  #deltaT^Kij
+        self.permitido = [i for i in range(grafo.rank)]
+        self.eta = [[  # 1/Lij
+            0 if i == j else 1 / grafo.matriz[i][j] for j in range(grafo.rank)
+        ] for i in range(grafo.rank)]
+        inicio = random.randint(0, grafo.rank - 1)  # inicio aleatório
+        self.tabu.append(inicio)
+        self.atual = inicio
+        self.permitido.remove(inicio)
+
+    def _seleciona_proximo(self):
+        denominador = 0
+        for j in self.permitido:
+            denominador += self.grafo.feromonio[
+                self.atual][j]**self.colonia.alfa * self.eta[
+                    self.atual][j]**self.colonia.beta
+        probabilidades = [
+            0 for i in range(self.grafo.rank)
+        ]  # probabilidades de mover para uma cidade no próximo passo
+        for i in range(self.grafo.rank):
+            try:
+                self.permitido.index(i)
+                probabilidades[i] = self.grafo.feromonio[self.atual][i] ** self.colonia.alfa * \
+                    self.eta[self.atual][i] ** self.colonia.beta / denominador
+            except ValueError:
+                pass   # descarta se a cidade nao for permitida
+
+        # seleciona a proxima cidade usando a técnica de roulette wheel
+        selecionado = 0
+        rand = random.random()
+        for i, probabilidade in enumerate(probabilidades):
+            rand -= probabilidade
+            if rand <= 0:
+                selecionado = i
+                break
+        self.permitido.remove(selecionado)
+        self.tabu.append(selecionado)
+        self.custo_total += self.grafo.matriz[self.atual][selecionado]
+        self.atual = selecionado
+
+
+def calc_distancia(cidade1, cidade2):
+     return math.sqrt((cidade1['x'] - cidade2['x'])**2 +
+                     (cidade1['y'] - cidade2['y'])**2)
+
+
+cities = [
+    {'index':0, 'x':10, 'y':20},
+    {'index':1, 'x':20, 'y':20},
+    {'index':2, 'x':30, 'y':20},
+]
+
+matriz_adjacencia = []
+rank = len(cities)
+for i in range(rank):  # calculo da matriz de adjacencia
+    linha = []
+    for j in range(rank):
+        linha.append(calc_distancia(cities[i], cities[j]))
+    matriz_adjacencia.append(linha)
+
+@app.route('/aco')
+def index():
+    aco = ACO(cont_formiga=10, geracoes=1, alfa=1.0, beta=10.0, ro=0.5, Q=10)
+    grafo = Grafo(matriz_adjacencia, rank)
+    caminho, custo = aco.resolve(grafo)
+    print(caminho)
+    return render_template('index.html')
+
+@app.route('/')
+def main_page():
+    return render_template("ardu.html")
+
+@flask_sijax.route(app, '/<int:req>')
 def ardu(req):
     if req == 100:
         with open("test.txt", 'w') as f:
@@ -64,19 +168,11 @@ def ardu(req):
           
     return render_template("ardu.html")
 
+
 @app.route('/about')
 def about():
-    return render_template('about.html', app_data=app_data)
+    return render_template('about.html')
 
-
-@app.route('/service')
-def service():
-    return render_template('service.html', app_data=app_data)
-
-
-@app.route('/contact')
-def contact():
-    return render_template('contact.html', app_data=app_data)
 
 
 if __name__ == '__main__':
